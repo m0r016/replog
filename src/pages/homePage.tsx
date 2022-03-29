7f368669984e2bc81cb5597b4504f82fc46342fd
m0r016 <yuuki12_25@icloud.com>
2022-03-30 00:03:17 +0900

    feat: ホームページ追加

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomePage = () => {
+    return (
+        <View>
+            <Text>ホーム画面</Text>
+        </View>
+    );
+};
+
+export default HomePage;
